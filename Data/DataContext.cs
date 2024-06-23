42e75856fbec17a3a5d4463299db4b3fce5225ac
Harsh Chandwani <harshchandwani50028@gmail.com>
2024-06-23 18:24:25 +0530

    Enabled Entity Framework and SQL Connection

---
+++ Data/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DbContext> options) : base(options)
+            //base is basically pushing the data to the DB
+        {
+            
+        }
+        //importing all the models
+        //always plural
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<PokemonOwner> PokemonsOwner { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+
+        //used to override the model creation, to customize the model creation process
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Define the entity PokemonCategory and specify a composite primary key.
+            // A composite primary key is made up of multiple columns, in this case, PokemonId and CategoryId.
+            modelBuilder.Entity<PokemonCategory>()
+                .HasKey(pc => new { pc.PokemonId, pc.CategoryId
+            });
+        }
+
+    }
+}
--- obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/PokemonReviewApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a04fdfffd198b90cfd53fe2e63c8891eac86a5c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b33511970ce267c48912a26acf05b88bacc550ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
